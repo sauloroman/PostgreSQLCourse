7e087ba82ca0c43779bf72a435ca32639f556c83
Saulo Roman Santillan Nava <romansantillan1998@outlook.com>
2025-04-19 10:24:04 -0600

    Section 05 done

--- exercises/05.exercise.sql
+++ exercises/05.exercise.sql
@@ -51,8 +51,41 @@ GROUP BY author_id
 
 -- An example of the authors table
 
+--* SOLUTION
 SELECT name, COUNT(*) as num_books_published
 FROM books
 INNER JOIN authors ON authors.id = books.author_id
 GROUP BY authors.name
 
+-- Practice Yourself Some Having
+-- You are trying to find the phone manufacturers with the greatest revenue from selling phones.
+
+-- Given a table of phones, print the names of manufacturers and total revenue (price * units_sold) for all phones.  Only print the manufacturers who have revenue greater than 2,000,000 for all the phones they sold.
+
+-- Hints:
+
+-- From the problem statement, it looks like you don't need to filter down any of the initial rows. That means you probably won't have to use the WHERE keyword!
+
+-- +-------------+--------------+-------+------------+
+-- | name        | manufacturer | price | units_sold |
+-- +-------------+--------------+-------+------------+
+-- | N1280       | Nokia        | 199   | 1925       |
+-- +-------------+--------------+-------+------------+
+-- | Iphone 4    | Apple        | 399   | 9436       |
+-- +-------------+--------------+-------+------------+
+-- | Galaxy S    | Samsung      | 299   | 2359       |
+-- +-------------+--------------+-------+------------+
+-- | S5620 Monte | Samsung      | 250   | 2385       |
+-- +-------------+--------------+-------+------------+
+-- | N8          | Nokia        | 150   | 7543       |
+-- +-------------+--------------+-------+------------+
+-- | Droid       | Motorola     | 150   | 8395       |
+-- +-------------+--------------+-------+------------+
+-- | Wave S8500  | Samsung      | 175   | 9259       |
+-- +-------------+--------------+-------+------------
+
+--* SOLUTION
+SELECT manufacturer, SUM( price * units_sold ) AS total_revenue
+FROM phones
+GROUP BY manufacturer 
+HAVING SUM(price * units_sold) > 2e6
